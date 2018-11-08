1e77ddaf2820c8f8b0ec71c1a3ce7edb7cae0c48
Dylan Kilkenny <dylankilkenny95@gmail.com>
2018-11-08 21:47:19 +0000

    public and private api routes

--- server/main.go
+++ server/main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	jwt_lib "github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go/request"
 	"github.com/dylankilkenny/watch-cash/server/controllers"
 	"github.com/dylankilkenny/watch-cash/server/db"
 	"github.com/gin-gonic/gin"
 )
 
+const secretkey = "verysecretkey1995"
+
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
 	db.Init()
-	router.GET("/", func(c *gin.Context) {
+
+	public := router.Group("/api")
+	private := router.Group("/api/private")
+
+	public.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"status":  200,
 			"message": "hello world",
 		})
 	})
-	router.POST("/signup", user.CreateUser)
+	public.POST("/signup", user.SignUpUser)
+	public.POST("/login", user.LoginUser)
+	private.Use(Auth(secretkey))
+	private.POST("/subscribe", user.SubscribeAddress)
 	router.Run()
+}
+
+func Auth(secret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		_, err := request.ParseFromRequest(c.Request, request.OAuth2Extractor, func(token *jwt_lib.Token) (interface{}, error) {
+			b := ([]byte(secret))
+			return b, nil
+		})
+
+		if err != nil {
+			c.AbortWithError(401, err)
+		}
+	}
 }
