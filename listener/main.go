dfbb0b089d0014e34ceaba8d8ac30846abf8f8e7
Dylan Kilkenny <dylankilkenny95@gmail.com>
2019-02-25 20:21:21 +0000

    seperate listener into its own service

--- listener/main.go
+++ listener/main.go
@@ -1,69 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"math"
+	"net/http"
+	"os"
 	"path/filepath"
 
-	bitdb "github.com/dylankilkenny/go-bitdb"
+	B "github.com/dylankilkenny/go-bitdb"
 	"github.com/dylankilkenny/watch-cash/listener/mail"
-	"github.com/dylankilkenny/watch-cash/server/user"
-	userModel "github.com/dylankilkenny/watch-cash/server/user/model"
+	"github.com/joho/godotenv"
 )
-
-func main() {
-	version := 3
-	bitsocketurl := "https://bitsocket.org/s/"
-	bitdburl := "https://bitdb.network/q/"
-
-	bitsocket := bitdb.NewSocket(version, bitsocketurl)
-	bitdb := bitdb.New(version, "qrqm04uwrd0wwaguxea079h0znswwt3quuejvl6zd6", bitdburl)
-
-	jq := ".[] | { txId: .tx.h, from: [.in[] | { prevTransactionId: .e.h, sender: \"bitcoincash:\\(.e.a)\" }], to: [.out[] | { receiver: \"bitcoincash:\\(.e.a?)\", amount: .e.v? }] }"
-	events, err := bitsocket.Stream("", jq)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	for event := range events {
-		if event.Type != "mempool" {
-			continue
-		}
-		tx := castTransaction(event.Data)
-		fmt.Println(tx)
-
-		for _, input := range tx.From {
-			users, err := user.SubscribedUsers(input.Sender[12:])
-			if err != nil {
-				fmt.Println(err)
-			}
-			for _, user := range users {
-				txhash := bitdb.TxHash(input.PrevTransactionID)
-				fmt.Println(txhash)
-
-				resp, err := bitdb.Request(txhash, ".[] | .out[0] | {amount: .e.v}")
-				if err != nil {
-					fmt.Println("BitDb", err)
-				}
-				confirmed, _ := resp.Confirmed.(map[string]interface{})
-				fmt.Println(confirmed)
-				bch := float64(confirmed["amount"].(float64)) / math.Pow10(int(8))
-				sendMail(user, input.Sender[12:], "sent", fmt.Sprintf("%f", bch), tx.TxID)
-			}
-		}
-		for _, output := range tx.To {
-			users, err := user.SubscribedUsers(output.Receiver[12:])
-			if err != nil {
-				fmt.Println(err)
-			}
-			bch := float64(output.Amount) / math.Pow10(int(8))
-			for _, user := range users {
-				sendMail(user, output.Receiver[12:], "recieved", fmt.Sprintf("%f", bch), tx.TxID)
-			}
-		}
-	}
-}
 
 type transaction struct {
 	TxID string `json:"txId"`
@@ -75,6 +25,125 @@ type transaction struct {
 		Receiver string `json:"receiver"`
 		Amount   int    `json:"amount"`
 	} `json:"to"`
+}
+
+type ApiResponse struct {
+	Status int      `json:"status"`
+	Error  ApiError `json:"errors"`
+	Data   []User   `json:"data"`
+}
+
+type ApiError struct {
+	Title  string `json:"title"`
+	Detail string `json:"detail"`
+}
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type TxInputAmount struct {
+	Amount int `json:"amount"`
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	version := 3
+	bitdburl := "https://bitdb.network/q/"
+	bitdb := B.New(version, "qrqm04uwrd0wwaguxea079h0znswwt3quuejvl6zd6", bitdburl)
+
+	bitsocketurl := "http://192.168.0.214:4000/s/"
+	bitsocket := B.NewSocket(version, bitsocketurl)
+
+	jq := ".[] | { txId: .tx.h, from: [.in[] | { prevTransactionId: .e.h, sender: .e.a }], to: [.out[] | { receiver: .e.a?, amount: .e.v? }] }"
+	events, err := bitsocket.Stream("", jq)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	for event := range events {
+		if event.Type != "mempool" {
+			continue
+		}
+
+		tx := castTransaction(event.Data)
+		fmt.Println("Data recieved:", tx)
+
+		checkTxInputs(tx, bitdb)
+		checkTxOutputs(tx)
+
+	}
+}
+
+func checkTxInputs(tx transaction, bitdb *B.Request) {
+	for _, input := range tx.From {
+		users := fetchUsers(input.Sender)
+		if len(users) == 0 {
+			continue
+		}
+
+		for _, user := range users {
+			txhash := bitdb.TxHash(input.PrevTransactionID)
+			resp, err := bitdb.Request(txhash, ".[] | .out[0] | {amount: .e.v}")
+			if err != nil {
+				fmt.Println("BitDb Request:", err)
+			}
+			confirmed, _ := resp.Confirmed.(map[string]interface{})
+			var amount TxInputAmount
+			amount = json.Unmarshal(resp.Confirmed, TxInputAmount)
+			bch := float64(confirmed["amount"].(float64)) / math.Pow10(int(8))
+			sendMail(user, input.Sender, "sent", fmt.Sprintf("%f", bch), tx.TxID)
+		}
+	}
+}
+
+func checkTxOutputs(tx transaction) {
+	for _, output := range tx.To {
+		users := fetchUsers(output.Receiver)
+		if len(users) == 0 {
+			continue
+		}
+		bch := float64(output.Amount) / math.Pow10(int(8))
+		for _, user := range users {
+			sendMail(user, output.Receiver, "recieved", fmt.Sprintf("%f", bch), tx.TxID)
+		}
+	}
+}
+
+func fetchUsers(address string) []User {
+
+	message := map[string]string{
+		"address": address,
+	}
+
+	bytesRepresentation, err := json.Marshal(message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "http://localhost:3001/api/secret/users", bytes.NewBuffer(bytesRepresentation))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("API_KEY", os.Getenv("WATCHCASHAPIKEY"))
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result ApiResponse
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	switch result.Status {
+	case 200:
+		return result.Data
+	default:
+		return nil
+	}
 }
 
 func castTransaction(data interface{}) transaction {
@@ -90,14 +159,14 @@ func castTransaction(data interface{}) transaction {
 	return tx
 }
 
-func sendMail(user userModel.User, address, txType, amount, txID string) {
+func sendMail(user User, address, txType, amount, txID string) {
 	newmail := mail.NewEmail([]string{user.Email})
 	absPath, _ := filepath.Abs("mail/template.html")
 	newmail.Send(absPath, map[string]string{
-		"username": user.FirstName,
-		"address":  address,
-		"txType":   txType,
-		"amount":   amount,
-		"txID":     txID,
+		"name":    user.Name,
+		"address": address,
+		"txType":  txType,
+		"amount":  amount,
+		"txID":    txID,
 	})
 }
